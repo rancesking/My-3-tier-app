baa553b5d97c1191c9d9e294fc566346b8d4e0be
Rances Acosta <racosta@applaudostudios.dev>
2022-07-08 08:55:56 -0400

    Midofied the dev.varsft file

--- outputs.tf
+++ outputs.tf
@@ -3,11 +3,6 @@ output "lb_dns_name" {
   value       = aws_lb.main.dns_name
 }
 
-output "aws_ecr_repository" {
-  description = "The URL of the ECR"
-  value       = aws_ecr_repository.app-repository.repository_url
-}
-
 output "back_dns_name" {
   description = "The DNS name of the backend load balancer"
   value       = aws_lb.back.dns_name
